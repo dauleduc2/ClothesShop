54630dcf12f90db3c24cba04325ec12d3e417187
unknown <kainesv86@gmail.com>
2021-09-24 15:23:21 +0700

    multerErrorMiddleware

--- backend/src/middlewares/multerErrorMiddleware.ts
+++ backend/src/middlewares/multerErrorMiddleware.ts
@@ -1,3 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import multer = require("multer");
+import upload from "../utils/multerHelper";
 
-module.exports = function (req: Request, res: Response, next: NextFunction) {};
+export const multerErrorMiddleware = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const handling = upload.array("images", 5);
+
+    handling(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            return res.status(418).send(Buffer.from(err.message));
+        } else if (err) {
+            return res.status(400).send("Something failed");
+        }
+        next();
+    });
+};
--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import upload from "../utils/multerHelper";
+// import  from "../middlewares/multerErrorMiddleware";
+import { multerErrorMiddleware } from "../middlewares/multerErrorMiddleware";
 import * as express from "express";
 const router = express.Router();
 router.get("/", async (req: Request, res: Response) => {
     res.send("hello");
 });
 
-router.post("/", upload.array("images", 5), (req: Request, res: Response) => {
-    res.send(req);
+// router.post("/", upload.array("images", 5), (req: Request, res: Response) => {
+router.post("/", multerErrorMiddleware, (req: Request, res: Response) => {
+    res.send(req.files);
 });
 export default router;
