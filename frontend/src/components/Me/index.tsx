5882d464258f40d89af8c1d03b1cdd40cf8719b5
unknown <kainesv86@gmail.com>
2021-09-30 08:06:22 +0700

    ADD - Me page

--- frontend/src/components/Me/index.tsx
+++ frontend/src/components/Me/index.tsx
@@ -1,7 +1,18 @@
+import Avatar from "@mui/material/Avatar";
+
 interface MePageProps {}
 
 const MePage: React.FunctionComponent<MePageProps> = () => {
-    return <div>Me Page</div>;
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="border-2 border-red-600 bg-white h-96 w-96">
+                Hello
+            </div>
+            <div className="flex items-center justify-center border-2 border-red-600 bg-white h-96 w-60">
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </div>
+        </div>
+    );
 };
 
 export default MePage;
