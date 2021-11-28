a28db619f2f0c8847ac0b18c371fea057faa23e1
Duc Dau <dauleduc2@gmail.com>
2021-11-28 14:15:36 +0700

    FIX static file and detele ormconfig

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -112,8 +112,10 @@ router.post(
     async (req: ServerRequest<LoginUserDTO>, res: Response) => {
         const { username, password } = req.body;
         //get connection
+        console.log("starting to login....");
         const userRepo = await getCustomRepository(UserRepository);
         const user: User = await userRepo.findByUsername(username);
+        console.log("got repo....");
         //validate
         const { error } = validateLoginUser(req.body);
         if (error) {
@@ -131,7 +133,7 @@ router.post(
                     )
                 );
         }
-
+        console.log("checking error....");
         //check valid username
         if (!user)
             return res
@@ -143,6 +145,7 @@ router.post(
                         "username or password is invalid"
                     )
                 );
+        console.log("checking duplicate....");
         //check valid password
         const isValidPassword = await bcrypt.compare(password, user.password);
         if (!isValidPassword)
@@ -208,6 +211,7 @@ router.post(
         const userRepo = await getCustomRepository(UserRepository);
         //check is esxisted user
         let isExistedUser = await userRepo.findByEmail(email);
+        console.log("check isExistedUser1" + isExistedUser);
         if (isExistedUser)
             duplicateField = {
                 ...duplicateField,
@@ -215,6 +219,7 @@ router.post(
             };
 
         isExistedUser = await userRepo.findByUsername(username);
+        console.log("check isExistedUser2" + isExistedUser);
         if (isExistedUser)
             duplicateField = {
                 ...duplicateField,
--- backend/src/startup/middleware.ts
+++ backend/src/startup/middleware.ts
@@ -5,6 +5,8 @@ import * as cookieParser from "cookie-parser";
 
 export default function (app: Application) {
     app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+    console.log(process.env.CLIENT_URL);
     app.use(cookieParser());
-    app.use(express.static("public"));
+    app.use(express.static("uploads"));
+    // app.use(express.static("public"));
 }
