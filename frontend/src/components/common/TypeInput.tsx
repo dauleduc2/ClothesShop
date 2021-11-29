fcca72786126d31d19eea661266a7eba57000793
Duc Dau <dauleduc2@gmail.com>
2021-11-29 18:38:38 +0700

    FIX - checking type of input field for update form

--- frontend/src/components/common/TypeInput.tsx
+++ frontend/src/components/common/TypeInput.tsx
@@ -41,7 +41,7 @@ const TypeInput: React.FunctionComponent<TypeInputProps> = ({
                     <div className="relative mt-1">
                         <Listbox.Button className="relative w-64 py-2 pl-3 pr-10 text-left bg-white border border-gray-300 rounded-md shadow-sm cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                             <span className="block truncate">
-                                {capitalizeFirstLetter(selectedType?.name.toLowerCase())}
+                                {selectedType ? capitalizeFirstLetter(selectedType?.name.toLowerCase()) : ''}
                             </span>
                             <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                 <SelectorIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
